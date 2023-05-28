96393a8895dcde12486015dfcd78a908d96e9931
혀느현스 <hyuns@hyuns.dev>
2023-05-28 16:58:21 +0900

    feat: add visitor 엔드포인트

--- entity/visitor.entity.ts
+++ entity/visitor.entity.ts
@@ -6,6 +6,12 @@ export class VisitorEntity extends BaseEntity {
   @Column({ type: 'varchar', length: 255 })
   visitorToken: string;
 
-  @Column({ type: 'int' })
+  @Column({ type: 'int', default: 1 })
   count: number;
+
+  constructor(data: { visitorToken: string }) {
+    super();
+    if (!data) return;
+    this.visitorToken = data.visitorToken;
+  }
 }
--- src/domain/visitor/visitor.service.ts
+++ src/domain/visitor/visitor.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { VisitorEntity } from 'entity/visitor.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
 
 @Injectable()
-export class VisitorService {}
+export class VisitorService {
+  constructor(
+    @InjectRepository(VisitorEntity)
+    private readonly visitorRepository: Repository<VisitorEntity>,
+  ) {}
+
+  async getVisitorsCount() {
+    const res = await this.visitorRepository
+      .createQueryBuilder('visitor')
+      .select(`SUM(visitor.count)`, 'count')
+      .getRawOne();
+
+    const count = +(res.count || 0);
+    return count;
+  }
+
+  async postVisitor(token?: string) {
+    let visitor = await this.visitorRepository.findOne({
+      where: {
+        visitorToken: token,
+      },
+    });
+
+    if (!visitor) {
+      visitor = new VisitorEntity({
+        visitorToken: await this.getNewToken(),
+      });
+      visitor = await this.visitorRepository.save(visitor);
+    } else {
+      visitor.count += 1;
+      await this.visitorRepository.save(visitor);
+    }
+
+    return {
+      visitorToken: visitor.visitorToken,
+      visitorsCount: await this.getVisitorsCount(),
+    };
+  }
+
+  private async getNewToken() {
+    return uuidV4();
+  }
+}
